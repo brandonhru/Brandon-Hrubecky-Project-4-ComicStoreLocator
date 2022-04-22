4be9fe54828880122d74697f8ba4101d60d44535
brandonhru <brandon.hru@gmail.com>
2022-04-22 00:04:27 -0400

    commit

--- routes/stores.js
+++ routes/stores.js
@@ -1,8 +1,10 @@
+//USING EXPRESS ROUTER
+//EXPORTING ROUTER
 const express = require('express');
 const { getStores, addStore } = require('../controllers/stores');
 
 const router = express.Router();
-
+//SLASH USED BECAUSE API IS ALREADY IDENTIFIED IN THE SERVER.JS FILE
 router.route('/').get(getStores).post(addStore);
 
 
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//EXPRESS CRASH COURSE USED FOR DEEPER UNDERSTANDING https://www.youtube.com/watch?v=L72fhGm1tfE&t=155s
+
 /* Setup Express */
 const path = require('path');
 const express = require('express');
@@ -5,7 +7,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-// load enviroment variables
+// load enviroment variables from the config file
 dotenv.config({ path: './config/config.env' });
 
 //Connect to Database
@@ -26,11 +28,6 @@ app.use(cors());
 
 // Routes
 app.use('/api/v1/stores', require('./routes/stores'));
-
-
-
-
-//app.listen(5000, '0.0.0.0')
 
 const PORT = process.env.PORT || 5000;
 
